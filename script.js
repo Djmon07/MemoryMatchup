7e8791332a04249ad5691b641c1254906b84d05c
Daniel Monville <djmonwork@gmail.com>
2019-12-05 11:31:40 -0500

    Added random number generator and class selector on div

--- script.js
+++ script.js
@@ -60,3 +60,20 @@ var cardType = {
   "Leopard"
   "Flamingo"
 }
+//create a class on the divs to target
+let tarDiv = document.body.getElementByClassName("");
+//random generator for cards
+let range = (start, end) => {
+  let array = [];
+  for (let i = start; i <= end; i ++) {
+  array.push(i);
+  }
+  return array;
+};
+let grid = (range(1, 61));
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+console.log(getRandomInt(grid.length));
+
+card = random(range)
