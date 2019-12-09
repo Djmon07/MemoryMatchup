5d01d2b65b947336dc9913f5f381f72594908493
Daniel Monville <djmonwork@gmail.com>
2019-12-09 12:43:44 -0500

    merged and changed the array from var

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
-var cardType1 = [
+let cardType1 = [
+  "Up",
+  "Down",
   "Up",
   "Down"
 ]
-var cardType2 = [
+let cardType2 = [
+  "Red",
+  "Blue",
+  "Green",
+  "Purple",
+  "Yellow",
+  "Orange",
+  "Pink",
+  "Indigo",
   "Red",
   "Blue",
   "Green",
@@ -12,7 +22,25 @@ var cardType2 = [
   "Pink",
   "Indigo"
 ]
-var cardType3 = [
+let cardType3 = [
+  "Carrots",
+  "Blueberries",
+  "Broccoli",
+  "Eggplant",
+  "Corn",
+  "Tomatoes",
+  "Watermelon",
+  "Plum",
+  "Cauliflower",
+  "Grapes",
+  "Strawberries",
+  "Cantaloupe",
+  "Peppers",
+  "Bananas",
+  "Apple",
+  "Peas",
+  "Mushrooms",
+  "Onions",
   "Carrots",
   "Blueberries",
   "Broccoli",
@@ -32,7 +60,39 @@ var cardType3 = [
   "Mushrooms",
   "Onions"
 ]
-var cardType4 = [
+let cardType4 = [
+  "Hippo",
+  "Lion",
+  "Tiger",
+  "Panda",
+  "Monkey",
+  "Ostrich",
+  "Wolf",
+  "Zebra",
+  "Elephant",
+  "Fox",
+  "Snake",
+  "Turtle",
+  "Lemur",
+  "Giraffe",
+  "Penguin",
+  "Polar Bear",
+  "Squid",
+  "Cat",
+  "Rabbit",
+  "Goat",
+  "Cow",
+  "Dog",
+  "Pig",
+  "Horse",
+  "Gecko",
+  "Rooster",
+  "Kangaroo",
+  "Ox",
+  "Boar",
+  "Sheep",
+  "Leopard",
+  "Flamingo",
   "Hippo",
   "Lion",
   "Tiger",
@@ -77,3 +137,27 @@ for (let x = 1; x <= cards; x++) {
 document.querySelector("input").addEventListener("click", event => {
 document.location.reload(true);
 })
+
+let counter = 0;
+window.addEventListener("mousedown", event => {
+ if (event.key == "click") {
+    counter++;
+    document.body.querySelectorAll('div') = "violet";
+ }
+  if (counter = 2 && cardType1() !== cardType1()) {
+    document.body.querySelectorAll('div') = "grey";
+    counter = 0;
+    event.disabled = "disabled";
+  }
+  if (document.body.querySelectorAll('div') = "grey") {
+    document.body.createTextNode("Congratulations! You Cleared This Level!");
+   document.body.style.color = "green";
+    divs[div].style.fontSize = "xx-large";
+    var duration = 5 seconds;
+    //This is where it would move on to the next level//
+  }
+  else {
+    var duration = 5 seconds;
+    document.body.querySelectorAll('div') = "lightblue";
+    counter = 0;
+  });
