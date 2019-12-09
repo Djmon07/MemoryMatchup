4baeee9fefce1170d5a1c29a8605d3c3720917dc
Daniel Monville <djmonwork@gmail.com>
2019-12-09 12:14:23 -0500

    Added div creater and randomizer for cards with different css setup to the page

--- script.js
+++ script.js
@@ -1,90 +1,71 @@
-var cardType = {
-  "Up"
-  "Down"
-  "Red"
-  "Blue"
-  "Green"
-  "Purple"
-  "Yellow"
-  "Orange"
-  "Pink"
-  "Indigo"
-  "Carrots"
-  "Blueberries"
-  "Broccoli"
-  "Eggplant"
-  "Corn"
-  "Tomatoes"
-  "Watermelon"
-  "Plum"
-  "Cauliflower"
-  "Grapes"
-  "Strawberries"
-  "Cantaloupe"
-  "Peppers"
-  "Bananas"
-  "Apple"
-  "Peas"
-  "Mushrooms"
-  "Onions"
-  "Hippo"
-  "Lion"
-  "Tiger"
-  "Panda"
-  "Monkey"
-  "Ostrich"
-  "Wolf"
-  "Zebra"
-  "Elephant"
-  "Fox"
-  "Snake"
-  "Turtle"
-  "Lemur"
-  "Giraffe"
-  "Penguin"
-  "Polar Bear"
-  "Squid"
-  "Cat"
-  "Rabbit"
-  "Goat"
-  "Cow"
-  "Dog"
-  "Pig"
-  "Horse"
-  "Gecko"
-  "Rooster"
-  "Kangaroo"
-  "Ox"
-  "Boar"
-  "Sheep"
-  "Leopard"
+var cardType = [
+  "Up",
+  "Down",
+  "Red",
+  "Blue",
+  "Green",
+  "Purple",
+  "Yellow",
+  "Orange",
+  "Pink",
+  "Indigo",
+  "Carrots",
+  "Blueberries",
+  "Broccoli",
+  "Eggplant",
+  "Corn",
+  "Tomatoes",
+  "Watermelon",
+  "Plum",
+  "Cauliflower",
+  "Grapes",
+  "Strawberries",
+  "Cantaloupe",
+  "Peppers",
+  "Bananas",
+  "Apple",
+  "Peas",
+  "Mushrooms",
+  "Onions",
+  "Hippo",
+  "Lion",
+  "Tiger",
+  "Panda",
+  "Monkey",
+  "Ostrich",
+  "Wolf",
+  "Zebra",
+  "Elephant",
+  "Fox",
+  "Snake",
+  "Turtle",
+  "Lemur",
+  "Giraffe",
+  "Penguin",
+  "Polar Bear",
+  "Squid",
+  "Cat",
+  "Rabbit",
+  "Goat",
+  "Cow",
+  "Dog",
+  "Pig",
+  "Horse",
+  "Gecko",
+  "Rooster",
+  "Kangaroo",
+  "Ox",
+  "Boar",
+  "Sheep",
+  "Leopard",
   "Flamingo"
+]
+
+var ul = document.querySelector("ul")
+
+let cards = 4;
+for (let x = 1; x <= cards; x++) {
+  var crDiv = document.createElement("div");
+  crDiv.textContent = cardType.splice(Math.floor(Math.random() * cardType.length), 1);
+  ul.appendChild(crDiv);
 }
-
-/* document.body.onload = addElement;
-
-function addElement () {
-  let newDiv = document.createElement("div");
-  newDiv.createElement(addElement);
-  let section = document.getElementByTagName("section");
-  section.appendChild(newDiv);
-}; */
-
-//div  target
-let section = document.getElementByTagName("SECTION");
-let tarDiv = section.getElementById("card");
-//random generator for cards
-let range = (start, end) => {
-  let array = [];
-  for (let i = start; i <= end; i ++) {
-  array.push(i);
-  }
-  return array;
-};
-let grid = (range(1, 3));
-function getRandomInt(max) {
-  return Math.floor(Math.random() * Math.floor(max));
-}
-var num = (getRandomInt(grid.length));
-var text = document.createTextNode(num);
-tarDiv.appendChild(text);
