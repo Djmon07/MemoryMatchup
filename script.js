97cb532c0dbbbe28824be542957219aac62e3d38
Daniel Monville <djmonwork@gmail.com>
2019-12-12 19:48:29 -0500

    added prevent dubble click and remove match on clicks

--- script.js
+++ script.js
@@ -68,7 +68,7 @@ let cardType4 = [
 ];
 
 let deck = [cardType1, cardType2, cardType3, cardType4];
-let deckNum = 0;
+let deckNum = 1;
 let ul = document.querySelector("ul");
 let count = 0;
 //get all the obj from array and create divs using each twice
@@ -83,78 +83,52 @@ function drawGrid(level) {
   for (let x = 0; x < length * 2; x++) {
     var crDiv = document.createElement("div");
     crDiv.textContent = cards.splice(Math.floor(Math.random() * cards.length), 1);
-    crDiv.setAttribute("class", "none");
+    crDiv.setAttribute("id", "none");
     ul.appendChild(crDiv);
   }
 };
 drawGrid(deck[deckNum]);
+
+//Click events
 let divAll = ul.querySelectorAll("div")
 let div = ul.querySelector("div")
-//Click events
-
 const clickHandeler = event => {
-    event.target.style.background = "violet";
-    event.target.style.color = "black";
-    console.log(event.target)
-    if (firstDiv == null) { // first click
-      firstDiv = event.target;
-      event.target.setAttribute("class", "clicked");
-    } else { // second click
-      if (event.target.textContent == firstDiv.textContent) {
-        console.log('we got a match', firstDiv);
-        event.target.style.background = "grey";
-        firstDiv.style.background = "grey";
-        firstDiv = null;
-      } else { //if not a match
-        let timedFlipBack = setTimeout(() => {
-          console.log('not a match');
-          event.target.style.background = "lightblue";
-          event.target.style.color = "transparent";
-          firstDiv.style.background = "lightblue";
-          firstDiv.style.color = "transparent";
-          firstDiv = null;
-        }, 2000);
-      }
-    }
+  event.target.style.background = "violet";
+  event.target.style.color = "black";
+  console.log(event.target)
+  if (firstDiv == null) { // first click
+    firstDiv = event.target;
+    firstDiv.style.display="block";
+    event.target.setAttribute("id", "clicked");
+  } else { // second click
+    if (event.target.textContent == firstDiv.textContent && event.target != firstDiv) {
+      console.log('we got a match', firstDiv);
+      console.log(event.target)
+      event.target.style.background = "grey";
+      firstDiv.style.background = "grey";
+      console.log(firstDiv.style.background,firstDiv)
+      firstDiv.style.display="none";
+      event.target.style.display="none"
+      firstDiv = null;
 
+
+    } else { //if not a match
+      let timedFlipBack = setTimeout(() => {
+        console.log('not a match');
+        event.target.style.background = "lightblue";
+        event.target.style.color = "transparent";
+        firstDiv.style.background = "lightblue";
+        firstDiv.style.color = "transparent";
+        firstDiv = null;
+      }, 200);
+    }
+  }
 };
 let firstDiv = null;
 let divs = Array.from(divAll);
-let length = divAll.length
+let length = divAll.length;
+console.log(divs);
 for (let y = 0; y < length; y++) {
-console.log(divs[y])
-divs[y].addEventListener("click", clickHandeler);
-}
-
-
-
-if (divAll.getElementsByClassName = ("clicked")) {
-  console.log("test")
-  console.log(divAll)
-}
-/* let divs = Array.from(divAll);
-let length = divAll.length
-console.log(divs)
-for (let y = 0; y < length; y++) {
-  let div = divAll[y];
-  console.log(div); */
-/* if div.getElementsByClassName("clicked") {
-    document.removeEventListener("click", clickHandeler);
-    console.log("test2");
-    console.log(div)
-}*/
-
-
-/* var element = ul.getElementsByClassName('clicked');
-element.addEventListener('click', event);*/
-
-//if document.body.ul.div.getElementsByClassName("clicked") {
-//  document.removeEventListener("click", event)
-//};
-
-//if div.element.id = "clicked") {
-//console.log("yes");
-//}
-//document.removeEventListener("click", event => {
-//if (event.target.nodeName == "DIV");
-//});
+  divs[y].addEventListener("click", clickHandeler);
+  console.log(divs[y]);
+};
