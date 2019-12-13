1c1d7a0761404cbf021b2fcfe1508c19c92f982f
Daniel Monville <djmonwork@gmail.com>
2019-12-12 21:14:58 -0500

    Started to add next level not finished

--- script.js
+++ script.js
@@ -67,8 +67,8 @@ let cardType4 = [
   "Flamingo"
 ];
 
-let deck = [cardType1, cardType2, cardType3, cardType4];
-let deckNum = 1;
+let deck = [cardType1, cardType2, cardType3, cardType4, 5];
+let deckNum = 0;
 let ul = document.querySelector("ul");
 let count = 0;
 //get all the obj from array and create divs using each twice
@@ -83,38 +83,39 @@ function drawGrid(level) {
   for (let x = 0; x < length * 2; x++) {
     var crDiv = document.createElement("div");
     crDiv.textContent = cards.splice(Math.floor(Math.random() * cards.length), 1);
-    crDiv.setAttribute("id", "none");
     ul.appendChild(crDiv);
   }
 };
 drawGrid(deck[deckNum]);
-
-//Click events
 let divAll = ul.querySelectorAll("div")
 let div = ul.querySelector("div")
+let divs = Array.from(divAll);
+let length = divAll.length;
+console.log(length)
+match = 0;
+//Click events
 const clickHandeler = event => {
   event.target.style.background = "violet";
   event.target.style.color = "black";
-  console.log(event.target)
   if (firstDiv == null) { // first click
     firstDiv = event.target;
-    firstDiv.style.display="block";
-    event.target.setAttribute("id", "clicked");
+    firstDiv.style.display = "block";
   } else { // second click
     if (event.target.textContent == firstDiv.textContent && event.target != firstDiv) {
-      console.log('we got a match', firstDiv);
-      console.log(event.target)
-      event.target.style.background = "grey";
-      firstDiv.style.background = "grey";
-      console.log(firstDiv.style.background,firstDiv)
-      firstDiv.style.display="none";
-      event.target.style.display="none"
+      firstDiv.style.display = "none";
+      event.target.style.display = "none"
       firstDiv = null;
-
-
+      match ++;
+      console.log(match)
+      if (match == length / 2){
+        deckNum ++;
+        drawGrid(deck[deckNum])
+        if ((drawGrid(deck[deckNum])) == 5) {
+          console.log("win")
+        }
+      }
     } else { //if not a match
       let timedFlipBack = setTimeout(() => {
-        console.log('not a match');
         event.target.style.background = "lightblue";
         event.target.style.color = "transparent";
         firstDiv.style.background = "lightblue";
@@ -125,10 +126,6 @@ const clickHandeler = event => {
   }
 };
 let firstDiv = null;
-let divs = Array.from(divAll);
-let length = divAll.length;
-console.log(divs);
 for (let y = 0; y < length; y++) {
   divs[y].addEventListener("click", clickHandeler);
-  console.log(divs[y]);
 };
